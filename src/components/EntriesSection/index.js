93239c6b416da99a0210c016ef89e29ff926065d
NAVSmith <56131958+NAVSmith@users.noreply.github.com>
2023-06-20 18:15:34 +0200

    puting some meet in the journal

--- src/components/EntriesSection/index.js
+++ src/components/EntriesSection/index.js
@@ -18,21 +18,27 @@ export default function EntriesSection() {
       </Tabs>
       <div className="entries-section__entries">
         <Entry
-          date="Feb 27, 2028"
-          motto="Thats life in the city"
-          notes="Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si mihi. Et quidem se repellere, idque instituit docere sic omne animal, simul atque."
+          date="Feb 19, 1428"
+          motto="Thats was it I guess"
+          notes="you heard me ."
         />
         <Divider />
         <Entry
-          date="Feb 26, 2028"
-          motto="Thats life in the city"
-          notes="Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si mihi. Et quidem se repellere, idque instituit docere sic omne animal, simul atque."
+          date="Feb 6, 1776"
+          motto="why do I live that long"
+          notes="It wasn't it I guess."
         />
         <Divider />
         <Entry
-          date="Feb 25, 2028"
-          motto="Thats life in the city"
-          notes="Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si mihi. Et quidem se repellere, idque instituit docere sic omne animal, simul atque."
+          date="Feb 5, 1928"
+          motto="using a journal for every 200 years is odd"
+          notes="still i'm using it."
+        />
+        <Divider />
+        <Entry
+          date="Feb 23, 1999"
+          motto="ho we are in the digital age"
+          notes="how can an electronical journal worked 500 years a go."
         />
       </div>
     </section>
