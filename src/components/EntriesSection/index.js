d3d98e5be5364d24eef5220a471445af39eb5261
NAVSmith <56131958+NAVSmith@users.noreply.github.com>
2023-06-21 12:31:02 +0200

    move the entries to a sperate file and made the in entries be made from the entries object

--- src/components/EntriesSection/index.js
+++ src/components/EntriesSection/index.js
@@ -1,9 +1,11 @@
+import {entries} from '../../data/entries.js'
 import "./EntriesSection.css";
 import Divider from "../Divider";
 import Entry from "../Entry";
 import Tabs from "../Tabs";
 import Tab from "../Tab";
 import Badge from "../Badge";
+import { Fragment } from 'react';
 
 export default function EntriesSection() {
   return (
@@ -17,30 +19,24 @@ export default function EntriesSection() {
         </Tab>
       </Tabs>
       <div className="entries-section__entries">
-        <Entry
-          date="Feb 19, 1428"
-          motto="Thats was it I guess"
-          notes="you heard me ."
-        />
-        <Divider />
-        <Entry
-          date="Feb 6, 1776"
-          motto="why do I live that long"
-          notes="It wasn't it I guess."
-        />
-        <Divider />
-        <Entry
-          date="Feb 5, 1928"
-          motto="using a journal for every 200 years is odd"
-          notes="still i'm using it."
-        />
-        <Divider />
-        <Entry
-          date="Feb 23, 1999"
-          motto="ho we are in the digital age"
-          notes="how can an electronical journal worked 500 years a go."
-        />
+        {entries.map((entry, index) => {
+          const dontAddDivider = index === 0 
+          return (
+            <Fragment key={index}>
+              {dontAddDivider? null: <Divider />}
+             <Entry motto={entry.motto} date={entry.date} notes={entry.notes} /> 
+            </Fragment>
+          )
+        })}
       </div>
     </section>
   );
 }
+
+/*
+- Map over the `entries` array and render each entry in the `EntriesSection` component.
+- Remember to use the `key` prop when mapping over the array.
+- If you are using this template: How can you render a `<Divider />` component between each entry?
+  - Don't break the flexbox layout (a `Fragment` might help)
+  - Avoid having a divider below the last or above the first entry
+*/
